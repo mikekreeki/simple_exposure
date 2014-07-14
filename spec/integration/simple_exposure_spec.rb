8bb304fa391931b6cacd1fd5fbfd5b633d3b8ac5
Michal Krejčí <mikekreeki@gmail.com>
2014-07-14 15:09:21 +0200

    Provide more comprehensive test approach

--- spec/integration/simple_exposure_spec.rb
+++ spec/integration/simple_exposure_spec.rb
@@ -4,18 +4,4 @@ describe SimpleExposure, type: :integration do
   it 'loads the modules' do
     expect(ActionController::Base.ancestors).to include(SimpleExposure::Core)
   end
-
-  let(:controller) { ApplicationController.new }
-
-  it 'can see the content' do
-    visit '/'
-
-    controller.index
-
-    expect(page).to have_content controller.send(:message)
-    expect(page).to have_content controller.send(:current_user)
-    expect(page).to have_content controller.send(:posts_count)
-  end
-
-
 end
